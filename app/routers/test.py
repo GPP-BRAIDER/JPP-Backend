d74541b4b96077caa736211f703343e4ea611983
peter <peter.odrobinak@outlook.skj>
2025-01-14 05:46:28 +0100

    update

--- app/routers/test.py
+++ app/routers/test.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.database.db import get_db
+from app.services.auth import oauth2
 
 router = APIRouter(
     prefix="/bloge",
@@ -15,6 +16,6 @@ router = APIRouter(
 #    return users
 
 @router.get('/')
-def all(db: Session = Depends(get_db)):
+def all(db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
     users = db.query(User).all()
     return users
